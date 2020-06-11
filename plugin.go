34129884a5ca769647444b74fa313eab9b4d02e5
collbrain <collbrain9@gmail.com>
2020-06-11 00:57:08 +0000

    changes made to handle UI version

--- plugin.go
+++ plugin.go
@@ -10,8 +10,16 @@ import (
 	"strings"
 	"strconv"
 	"github.com/olekukonko/tablewriter"
-    "sort"
+	"sort"
 )
+
+type Error struct {
+	Type       string `json:"type"`
+	Category   string `json:"category"`
+	Severity   int    `json:"priority"`
+	Msg        string `json:"message"`
+}
+
 
 type PopeyePlugin struct {
 	Name string
@@ -32,13 +40,13 @@ func NewPlugin(cluster *string) *PopeyePlugin {
 	if err != nil {
 		return nil
 	}
-	
+
 	plug := PopeyePlugin{}
 	plug.Pop = pop
 	return &plug
 }
 
-func (p *PopeyePlugin) PrintDump() {
+func (p *PopeyePlugin) GetHappyCLIVisibilityReport() {
 	if err := p.Pop.Init(); err != nil {
 		return
 	}
@@ -49,7 +57,61 @@ func (p *PopeyePlugin) PrintDump() {
 	p.Pop.Dump(true)
 }
 
-func (p *PopeyePlugin) PrintErrors(w *tablewriter.Table) {
+func (p *PopeyePlugin) GettExpeditionCLIAndUIVisibilityReport() string {
+	if err := p.Pop.Init(); err != nil {
+		return ""
+	}
+	if err := p.Pop.Sanitize(); err != nil {
+		return ""
+	}
+	k, _ := p.Pop.Builder.ToJSON()
+	return k
+}
+
+func (p *PopeyePlugin) GetExpeditionCLIAndUIVisibilityErrors() []*Error {
+	result := make([]*Error, 0)
+	if err := p.Pop.Init(); err != nil {
+		return nil
+	}
+	if err := p.Pop.Sanitize(); err != nil {
+		return nil
+	}
+	p.Pop.Builder.ToJSON()
+	codes, _ := issues.LoadCodes()
+	rows := make(map[int][]*Error)
+	for _,s := range p.Pop.Builder.Report.Sections {
+		for k,v := range s.Outcome {
+			for _,issue := range v {
+				if(issue.Level == 3) {
+					row := PopeyToAction(s.Title, k, issue.Message, codes)
+					if len(row) == 4 {
+						//w.Append(row)
+						code,_ := strconv.Atoi(row[2])
+						errList := rows[code]
+						errList = append(errList, getNewError(s.Title, k, issue.Message, codes))
+						rows[code] = errList
+					}
+				}
+			}
+		}
+	}
+
+	keys := make([]int, 0)
+	for k, _ := range rows {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	for _, k := range keys {
+		errList := rows[k]
+		for _, err := range errList {
+			result = append(result, err)
+		}
+	}
+	return result
+}
+
+func (p *PopeyePlugin) GetHappyCLIVisibilityErrors(w *tablewriter.Table) {
 	if err := p.Pop.Init(); err != nil {
 		return
 	}
@@ -57,34 +119,34 @@ func (p *PopeyePlugin) PrintErrors(w *tablewriter.Table) {
 		return
 	}
 	p.Pop.Builder.ToJSON()
-    codes, _ := issues.LoadCodes()
-    w.SetHeader([]string{"Section", "Item", "Priority", "Message"})
-    rows := make(map[int][][]string)
-    for _,s := range p.Pop.Builder.Report.Sections {
+	codes, _ := issues.LoadCodes()
+	w.SetHeader([]string{"Section", "Item", "Priority", "Message"})
+	rows := make(map[int][][]string)
+	for _,s := range p.Pop.Builder.Report.Sections {
+		for k,v := range s.Outcome {
+			for _,issue := range v {
+				if(issue.Level == 3) {
+					row := PopeyToAction(s.Title, k, issue.Message, codes)
+					if len(row) == 4 {
+						//w.Append(row)
+						code,_ := strconv.Atoi(row[2])
+						rows[code] = append(rows[code],row)
+					}
+				}
+			}
+		}
+	}
 
-        for k,v := range s.Outcome {
-            for _,issue := range v {
-                if(issue.Level == 3) {
-                    row := PopeyToAction(s.Title, k, issue.Message, codes)
-                    if len(row) == 4 {
-                        //w.Append(row)
-                        code,_ := strconv.Atoi(row[2])
-                        rows[code] = append(rows[code],row)
-                    }
-                }
-            }
-        }
-    }
-    keys := make([]int, 0)
-    for k, _ := range rows {
-        keys = append(keys, k)
-    }
-    sort.Ints(keys)
-    for _, k := range keys {
-        w.AppendBulk(rows[k])
-    }
-    w.Render()
+	keys := make([]int, 0)
+	for k, _ := range rows {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
 
+	for _, k := range keys {
+		w.AppendBulk(rows[k])
+	}
+	w.Render()
 }
 
 
@@ -99,15 +161,24 @@ func PopeyCodeFromMsg(msg string) (config.ID,string,error) {
         }
         return config.ID(imsg),msg[stop+2:],nil
 }
+
 func PopeyToAction(section string, item string, msg string, codes *issues.Codes) []string{
-    
     code, new_msg, err := PopeyCodeFromMsg(msg)
     if err == nil {
-    		// fmt.Fprintln(w, section, item, "Error:")
+			// fmt.Fprintln(w, section, item, "Error:")
             // fmt.Fprintf(w, "Severity: %v : %v\n\n", codes.Glossary[code].TailwindSeverity, new_msg)
             return []string{section, item, strconv.Itoa(int(codes.Glossary[code].TailwindSeverity)), new_msg}
     } else {
             fmt.Errorf(err.Error())
     }
     return []string{}
+}
+
+func getNewError(section string, item string, msg string, codes *issues.Codes) *Error {
+	code, new_msg, err := PopeyCodeFromMsg(msg)
+	if err == nil {
+		e := Error{section, item, int(codes.Glossary[code].TailwindSeverity), new_msg}
+		return &e
+	}
+	return &Error{}
 }
